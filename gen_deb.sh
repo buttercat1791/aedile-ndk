e3f5ce98f321ae59411ca7a944f82e0dc3c30049
Finrod Felagund <finrod.felagund.97@gmail.com>
2024-06-21 17:39:50 +0200

    create symbolic link to /usr/ directory to make installation usable

--- gen_deb.sh
+++ gen_deb.sh
@@ -4,17 +4,27 @@ export PACKAGE_NAME="libaedile-dev"
 export VERSION="0.0.2"
 
 export SRC_ROOT_DIR=$(dirname $(readlink -f ${0}))
-export PACK_DIR="${SRC_ROOT_DIR}/deb/opt/${PACKAGE_NAME}/usr"
-echo $PACK_DIR
+export DEB_PACKAGE_ROOT="${SRC_ROOT_DIR}/deb"
+export INSTALL_DIR="${DEB_PACKAGE_ROOT}/opt/${PACKAGE_NAME}/usr"
 
 
-rm -rf $PACK_DIR
-mkdir -p $PACK_DIR
+rm -rf $INSTALL_DIR
+mkdir -p $INSTALL_DIR
 
 cd "${SRC_ROOT_DIR}"
 cmake --preset=linux
 cmake --build build/linux
 cmake --install build/linux
+
+rm -rf ${DEB_PACKAGE_ROOT}/usr/
+
+mkdir -p ${DEB_PACKAGE_ROOT}/usr/include/aedile/client
+mkdir -p ${DEB_PACKAGE_ROOT}/usr/lib
+
+ln -s ${INSTALL_DIR}/include/nostr.h ${DEB_PACKAGE_ROOT}/usr/include/aedile/nostr.h
+ln -s ${INSTALL_DIR}/include/nostr.hpp ${DEB_PACKAGE_ROOT}/usr/include/aedile/nostr.hpp
+ln -s ${INSTALL_DIR}/include/client/web_socket_client.hpp ${DEB_PACKAGE_ROOT}/usr/include/aedile/client/web_socket_client.hpp
+ln -s ${INSTALL_DIR}/lib/libaedile.a ${DEB_PACKAGE_ROOT}/usr/lib/libaedile.a
 
 cd "${SRC_ROOT_DIR}"
 
