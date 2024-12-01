f58bc1a1e3ba130b8cc9263309d491a83d807725
buttercat1791 <mjjurkoic@gmail.com>
2024-12-01 09:04:54 -0600

    Split `_printNoscryptError` definition into .cpp file

--- src/internal/noscrypt_logger.hpp
+++ src/internal/noscrypt_logger.hpp
@@ -10,34 +10,4 @@
 */
 #define NC_LOG_ERROR(result) _printNoscryptError(result, __func__, __LINE__)
 
-static void _printNoscryptError(NCResult result, const std::string funcName, int lineNum)
-{
-    uint8_t argPosition;
-
-    switch (NCParseErrorCode(result, &argPosition))
-    {
-    case E_NULL_PTR:
-        PLOG_ERROR << "noscrypt - error: A null pointer was passed in " << funcName << "(" << argPosition << ") at line " << lineNum;
-        break;
-
-    case E_INVALID_ARG:
-        PLOG_ERROR << "noscrypt - error: An invalid argument was passed in " << funcName << "(" << argPosition << ") at line " << lineNum;
-        break;
-
-    case E_INVALID_CONTEXT:
-        PLOG_ERROR << "noscrypt - error: An invalid context was passed in " << funcName << "(" << argPosition << ") on line " << lineNum;
-        break;
-
-    case E_ARGUMENT_OUT_OF_RANGE:
-        PLOG_ERROR << "noscrypt - error: An argument was out of range in " << funcName << "(" << argPosition << ") at line " << lineNum;
-        break;
-
-    case E_OPERATION_FAILED:
-        PLOG_ERROR << "noscrypt - error: An operation failed in " << funcName << "(" << argPosition << ") at line " << lineNum;
-        break;
-
-    default:
-        PLOG_ERROR << "noscrypt - error: An unknown error " << result << " occurred in " << funcName << "(" << argPosition << ") at line " << lineNum;
-        break;
-    }
-}
+void _printNoscryptError(NCResult result, const char *func, int line);
